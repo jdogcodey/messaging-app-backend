344af6764f6f1e4d704e593728902ff9e48c9219
Jonathan Dawson <jonnydawson@hotmail.com>
2025-08-29 15:05:58 +0100

    chore: workaround for __dirname

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import express from "express";
 import "dotenv/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 // Defining the PORT from the .env
 const PORT = process.env.PORT || 3000;
+
+// Workaround for __dirname in ES modules
+const __filename = fileURL;
