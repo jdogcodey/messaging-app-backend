36bc66962bc119772ccf62355494a4da10bb46e0
Jonathan Dawson <jonnydawson@hotmail.com>
2025-08-29 21:08:17 +0100

    chore: set up router and controllers

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import cors from "cors";
+import routes from "./routes/indexRouter.js";
 
 // Defining the PORT from the .env
 const PORT = process.env.PORT || 3000;
@@ -20,3 +21,33 @@ app.use(
     origin: "http://localhost:5173",
   })
 );
+
+//Middleware to parse JSON request bodies
+app.use(express.json());
+
+//Middleware to parse URL-encoded form submissions
+app.use(
+  express.urlencoded({ extended: true, limit: "1mb", parameterLimit: 5000 })
+);
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Use the router
+app.use(routes);
+
+// 404 Error Handler - Handles unknown routes
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+// 500 Error Handler - Catches and logs server Errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res
+    .status(500)
+    .json({ error: "Something went wrong, please try again later" });
+});
+
+// Start the server
+app.listen(PORT, () => console.log(`App listening on ${PORT}`));
