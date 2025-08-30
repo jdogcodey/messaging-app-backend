6701145c888f2ca97f15e4cdda68c4602f90b49a
Jonathan Dawson <jonnydawson@hotmail.com>
2025-08-30 22:28:55 +0100

    chore: set test file to clear db before running tests

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,2 +1,17 @@
 import request from "supertest";
 import app from "../app.js";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+beforeEach(async () => {
+  // Reset DB
+  await prisma.messageRecipient.deleteMany({});
+  await prisma.message.deleteMany({});
+  await prisma.user.deleteMany({});
+});
+
+afterAll(async () => {
+  // Exit the db cleanly
+  await prisma.$disconnect();
+});
