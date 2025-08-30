8f9e72886c91c8d326a80c12ed1ff9f77b16d619
Jonathan Dawson <jonnydawson@hotmail.com>
2025-08-30 22:11:39 +0100

    feat: separate server from app for testing

--- app.js
+++ app.js
@@ -5,43 +5,33 @@ import { fileURLToPath } from "node:url";
 import cors from "cors";
 import routes from "./routes/indexRouter.js";
 
-// Defining the PORT from the .env
-const PORT = process.env.PORT || 3000;
+const app = express();
 
 // Workaround for __dirname in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Creating the express app
-const app = express();
-
-//Allowing cross-site access for the frontend
+// Middleware
 app.use(
   cors({
     origin: "http://localhost:5173",
   })
 );
-
-//Middleware to parse JSON request bodies
 app.use(express.json());
-
-//Middleware to parse URL-encoded form submissions
 app.use(
   express.urlencoded({ extended: true, limit: "1mb", parameterLimit: 5000 })
 );
-
-// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// Use the router
+// Routes
 app.use(routes);
 
-// 404 Error Handler - Handles unknown routes
+// 404
 app.use((req, res, next) => {
   res.status(404).json({ error: "Route not found" });
 });
 
-// 500 Error Handler - Catches and logs server Errors
+// 500
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res
@@ -49,5 +39,4 @@ app.use((err, req, res, next) => {
     .json({ error: "Something went wrong, please try again later" });
 });
 
-// Start the server
-app.listen(PORT, () => console.log(`App listening on ${PORT}`));
+export default app;
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,9 @@
 import express from "express";
 import indexController from "../controllers/indexController.js";
+import validationController from "../controllers/validationController.js";
 
 const router = express.Router();
+
+router.post("/signup", validationController.signup(), indexController.signup);
 
 export default router;
