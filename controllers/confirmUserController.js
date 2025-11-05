5fdc977cb699f6dc782ce85fbb84f129b45de5e9
JDogCodey <jonnydawson@hotmail.com>
2025-11-05 17:48:42 +0000

    feat(messages): POST /message/:receiverId rejects if sender is same as receiver with 400

--- controllers/confirmUserController.js
+++ controllers/confirmUserController.js
@@ -1,5 +1,16 @@
-function confirmUserController(req, res, next) {
-  return res.status(404).json({});
+import prisma from "../config/prisma-client";
+
+async function confirmUserController(req, res, next) {
+  // Request the receiver from the database
+  const receiver = await prisma.user.findUnique({
+    where: {
+      id: req.params.receiverId,
+    },
+  });
+  //
+  if (!receiver) {
+    return res.status(404).json({});
+  } else return res.status(400).json({});
 }
 
 export default confirmUserController;
