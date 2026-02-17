2e262d340a27378179805c1ccf92a7bb14646552
JDogCodey <jonnydawson@hotmail.com>
2026-02-17 13:25:05 +0000

    feat(friends): GET /user-search returns 200 once logged in

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -259,6 +259,12 @@ const indexController = {
     })
   }
   },
+  userSearch: async (req, res, next) => {
+    res.status(200).json({
+      success: true,
+      message: "User search results"
+    })
+  },
 };
 
 export default indexController;
